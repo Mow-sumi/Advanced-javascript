bbfbef6721fb1c4a7152463e4333af3ac0c2bc3d
mow-sumi <mow521202sumi@gmail.com>
2021-02-01 23:14:36 +0600

    null vs undefined

---
+++ nullvsundefined.js
@@ -0,0 +1,20 @@
+
+ function add(num1, num2){
+console.log(num1 + num2);
+ return
+} 
+
+function add(num1, num2){
+    console.log(num1 , num2);
+}
+
+
+
+const user = {name:"mowsumi",phone:01796}; 
+console.log(user.address);
+
+let fun = undefined;
+console.log (fun);
+
+let ages = [2,5,9]
+console.log(ages[11]);
