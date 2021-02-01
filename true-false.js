583e5a5c6a731d0418d315c056f32a8241f4b5d3
mow-sumi <mow521202sumi@gmail.com>
2021-02-01 22:47:43 +0600

    trutuy

---
+++ true-false.js
@@ -0,0 +1,21 @@
+let name =null;
+//console.log(name);
+if(name){
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
+    
+// falsy value
+// 0
+// ""
+//undefined
+// null
+// NaN
+
+// truthy value
+// '0'
+// ' '
+// []
